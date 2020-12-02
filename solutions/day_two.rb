602834765ca6bdec36f30d4ca6e6da37e40eae2e
Rachael Wright-Munn <chaelcodes@gmail.com>
2020-12-02 23:40:05 +0000

    Solution for Advent of Code Day 2

---
+++ solutions/day_two.rb
@@ -0,0 +1,36 @@
+require 'pry'
+
+class DayTwo
+    FILE_INPUT = "./inputs/day_two.txt"
+
+    def initialize(input, part)
+        @input = input
+        @part = part
+    end
+
+    def process
+        @input.count { |password| valid_password(password) }
+    end
+
+    def valid_password(password)
+        parsed_pword = password.match(/(?<min>\d+)-(?<max>\d+) (?<match>\w): (?<password>\w+)/)
+        min, max, match, password = parsed_pword[:min].to_i, parsed_pword[:max].to_i, parsed_pword[:match], parsed_pword[:password]
+        
+        if (@part == 1)
+            count = password.count(match)
+            count >= min && count <= max
+            # parsed_pword[:password].match?(/(?=(?:[^#{match}]*[#{match}][^#{match}]*){#{min},#{max}})/)
+            # parsed_pword[:password].match?(/[^#{match}]*(#{match}[^#{match}]*){#{min},#{max}}/)
+        else
+            char_1 = password[min - 1]
+            char_2 = password[max - 1]
+            [char_1, char_2].include?(match) && char_1 != char_2
+        end
+    end
+
+    def self.run(part)
+        file = File.read(FILE_INPUT)
+        array = file.split("\n")
+        DayTwo.new(array, part).process
+    end
+end
