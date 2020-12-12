8fa739e3c3cc34b425f2b14c374ff42ef584a959
Rachael Wright-Munn <chaelcodes@gmail.com>
2020-12-12 15:52:54 +0000

    Part 1

--- solutions/day_eight.rb
+++ solutions/day_eight.rb
@@ -4,8 +4,33 @@ require 'pry'
 class DayEight
     include Solution
     FILE_INPUT = "./inputs/day_eight.txt"
+    
+    attr_accessor :acc, :index
+    
+    def initialize(input, part)
+        super
+        self.index = 0
+        self.acc = 0
+    end
+
+    def exec(line)
+        instruction = @input[line].match(/(?<operation>\w{3}) (?<argument>\+?-?\d+)/)
+        increment_instruction = 1
+        case instruction[:operation]
+        when 'acc'
+            self.acc += instruction[:argument].to_i
+        when 'jmp'
+            increment_instruction = instruction[:argument].to_i
+        end
+        self.index += increment_instruction
+    end
 
     def process
-        @input
+        instructions_called = []
+        until instructions_called.include?(index)
+            instructions_called << index
+            exec(index)
+        end
+        acc
     end
 end
--- spec/solutions/day_eight_spec.rb
+++ spec/solutions/day_eight_spec.rb
@@ -7,7 +7,7 @@ RSpec.describe DayEight do
 
         context 'part 1' do
             let(:part) { 1 }
-            it { is_expected.to eq 0 }
+            it { is_expected.to eq 1723 }
         end
 
         context 'when part 2' do
@@ -17,15 +17,86 @@ RSpec.describe DayEight do
 
     describe '#process' do
         subject { described_class.new(input, part).process }
-        let(:input) { %w[hello] }
+        let(:input) { ["nop +0",
+                        "acc +1",
+                        "jmp +4",
+                        "acc +3",
+                        "jmp -3",
+                        "acc -99",
+                        "acc +1",
+                        "jmp -4",
+                        "acc +6"] }
 
         context 'part 1' do
             let(:part) { 1 }
-            it { is_expected.to eq 0 }
+            it { is_expected.to eq 5 }
         end
 
         context 'when part 2' do
             let(:part) { 2 }
+        end
+    end
+
+    describe '#exec' do
+        let(:day_eight) { described_class.new(input, part) }
+        let(:input) { ["nop +0",
+                        "acc +1",
+                        "jmp +4",
+                        "acc +3",
+                        "jmp -3",
+                        "acc -99",
+                        "acc +1",
+                        "jmp -4",
+                        "acc +6"] }
+        subject { day_eight.exec(line) }
+        context 'when part 1' do
+            let(:part) { 1 }
+
+            context 'when nop instruction' do
+                let(:line) { 0 }
+
+                it 'does nothing' do
+                    expect { subject }.to change(day_eight, :index).by(1)
+                    expect(day_eight.acc).to eq 0
+                end
+            end
+
+            context 'when acc instruction' do
+                let(:line) { 1 }
+
+                it 'increments acc' do
+                    expect { subject }.to change(day_eight, :index).by(1)
+                    expect(day_eight.acc).to eq 1
+                end
+
+                context 'with negative acc instruction' do
+                    let(:line) { 5 }
+
+                    it 'decrements acc' do
+                        expect { subject }.to change(day_eight, :index).by(1)
+                        expect(day_eight.acc).to eq -99
+                    end
+                end
+            end
+
+            context 'when jmp instruction' do
+                let(:line) { 2 }
+                
+                it 'increments index' do
+                    expect { subject }.not_to change(day_eight, :acc)
+                    expect(day_eight.index).to eq 4
+                end
+
+                context 'with negative jmp instruction' do
+                    let(:line) { 4 }
+
+                    it 'decrements index' do
+                        day_eight.index = 4
+                        expect { subject }.not_to change(day_eight, :acc)
+                        expect(day_eight.index).to eq 1
+                    end
+                end
+            end
         end
     end
 end
