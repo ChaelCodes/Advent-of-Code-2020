0077ce927d6a93c59726d35325f1c2f41ffe003e
Rachael Wright-Munn <chaelcodes@gmail.com>
2020-12-15 14:27:43 +0000

    The solution to Part 1

--- solutions/day_ten.rb
+++ solutions/day_ten.rb
@@ -1,10 +1,24 @@
 require_relative 'solution'
+require 'pry'
 
 class DayTen
     include Solution
     FILE_INPUT = "./inputs/day_ten.txt"
 
+    def self.parse_line(line)
+        line.to_i
+    end
+
     def process
-        @input.first
+        differences = [3] # add the device difference
+        sorted_input = @input.sort { |a, b| b <=> a }
+        sorted_input << 0 # Add the plug
+        sorted_input.each_with_index do |joltage, index|
+            next if joltage.zero?
+            differences << joltage - sorted_input[index + 1]
+        end
+        threes = differences.count{_1 == 3}
+        ones = differences.count{_1 == 1}
+        threes * ones
     end
 end
--- spec/solutions/day_ten_spec.rb
+++ spec/solutions/day_ten_spec.rb
@@ -3,7 +3,7 @@ require './solutions/day_ten'
 
 RSpec.describe DayTen do
     let(:instance) { described_class.new(input, part) }
-    let(:input) { [] }
+    let(:input) { [16, 10, 15, 5, 1, 11, 7, 19, 6, 12, 4] }
 
     describe '.run' do
         subject { described_class.run(part) }
@@ -11,6 +11,7 @@ RSpec.describe DayTen do
         context 'part 1' do
             let(:part) { 1 }
 
+            it { is_expected.to eq 2516 }
         end
 
         context 'when part 2' do
@@ -24,6 +25,14 @@ RSpec.describe DayTen do
 
         context 'part 1' do
             let(:part) { 1 }
+
+            it { is_expected.to eq 35 }
+
+            context 'longer input' do
+                let(:input) { [28, 33, 18, 42, 31, 14, 46, 20, 48, 47, 24, 23, 49, 45, 19, 38, 39, 11, 1, 32, 25, 35, 8, 17, 7, 9, 4, 2, 34, 10, 3] }
+
+                it { is_expected.to eq 220 }
+            end
         end
 
         context 'when part 2' do
