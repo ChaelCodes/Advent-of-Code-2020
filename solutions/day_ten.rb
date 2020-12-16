676ff3ecbe4768df9464b3c06c9ca7c265af53d8
Rachael Wright-Munn <chaelcodes@gmail.com>
2020-12-16 04:20:40 +0000

    Part 2 solution

--- solutions/day_ten.rb
+++ solutions/day_ten.rb
@@ -17,8 +17,32 @@ class DayTen
             next if joltage.zero?
             differences << joltage - sorted_input[index + 1]
         end
-        threes = differences.count{_1 == 3}
-        ones = differences.count{_1 == 1}
-        threes * ones
+        if @part == 1
+            threes = differences.count{_1 == 3}
+            ones = differences.count{_1 == 1}
+            return threes * ones
+        end
+        replaceables = []
+        index = 0
+        while(index < differences.length) do
+            if differences[index] == 1
+                count = 0
+                while (differences[index] == 1)
+                    count += 1
+                    index += 1
+                end
+                replaceables << count
+            end
+            index += 1
+        end
+        replaceables.map { convert_to_arrangements(_1) }.reduce(&:*)
+    end
+
+    def convert_to_arrangements(repeats)
+        base = 2**(repeats - 1)
+        return base unless repeats > 3
+        return 22 if repeats == 6
+        return 13 if repeats == 5
+        return 7 if repeats == 4
     end
 end
--- spec/solutions/day_ten_spec.rb
+++ spec/solutions/day_ten_spec.rb
@@ -17,6 +17,7 @@ RSpec.describe DayTen do
         context 'when part 2' do
             let(:part) { 2 }
 
+            it { is_expected.to eq 296196766695424 }
         end
     end
 
@@ -37,6 +38,44 @@ RSpec.describe DayTen do
 
         context 'when part 2' do
             let(:part) { 2 }
+
+            it { is_expected.to eq 8 }
+
+            context 'longer input' do
+                let(:input) { [28, 33, 18, 42, 31, 14, 46, 20, 48, 47, 24, 23, 49, 45, 19, 38, 39, 11, 1, 32, 25, 35, 8, 17, 7, 9, 4, 2, 34, 10, 3] }
+
+                it { is_expected.to eq 19208 }
+            end
+        end
+    end
+
+    describe '#convert_to_arrangements' do
+        subject { instance.convert_to_arrangements(repeats) }
+        let(:part) { 2 }
+
+        context 'repeats' do
+            let(:repeats) { 1 }
+            it { is_expected.to eq 1 }
+        end
+        context 'repeats' do
+            let(:repeats) { 2 }
+            it { is_expected.to eq 2 }
+        end
+        context 'repeats' do
+            let(:repeats) { 3 }
+            it { is_expected.to eq 4 }
+        end
+        context 'repeats' do
+            let(:repeats) { 4 } # -1 (1) 2**4 - 2**3 -1
+            it { is_expected.to eq 7 }
+        end
+        context 'repeats' do
+            let(:repeats) { 5 } # -3 (1) 2**5 - 2**4 -3, 2**4 - 2**1 -1
+            it { is_expected.to eq 13 }
+        end
+        context 'repeats' do
+            let(:repeats) { 6 } # -10 (2) 2**5 - 2**3 -2
+            it { is_expected.to eq 22 }
         end
     end
 end
