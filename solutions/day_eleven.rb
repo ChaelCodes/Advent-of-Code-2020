6cedb4e8bd7c34b583d76242db2813730a72b53a
Rachael Wright-Munn <chaelcodes@gmail.com>
2020-12-25 01:44:18 +0000

    Part 1 for Day Eleven

--- solutions/day_eleven.rb
+++ solutions/day_eleven.rb
@@ -1,10 +1,68 @@
 require_relative 'solution'
+require 'pry'
 
 class DayEleven
     include Solution
     FILE_INPUT = "./inputs/day_eleven.txt"
+    EMPTY = 'L'.freeze
+    OCCUPIED = '#'.freeze
+    FLOOR = '.'.freeze
+
+    attr_accessor :input
+
+    def initialize(input, part)
+        self.input = input
+        @part = part
+    end
 
     def process
-        @input.first
+        while process_seat_map do
+            nil
+        end
+        input.sum { _1.count(OCCUPIED) }
+    end
+
+    def process_seat_map
+        temp_map = input.map(&:dup)
+        changed = false
+        (0...input.length).each do |y|
+            (0...input[0].length).each do |x|
+                result = seat_transformation(x, y)
+                if (result == 1)
+                    changed = true
+                    temp_map[y][x] = OCCUPIED
+                elsif (result == -1)
+                    changed = true
+                    temp_map[y][x] = EMPTY
+                end
+            end
+        end
+        self.input = temp_map
+        changed
+    end
+
+    def seat_transformation(x, y)
+        current = input[y][x]
+        return 0 if current == FLOOR
+        neighbors = count_neighbors(x, y)
+        return -1 if neighbors >= 4 && current == OCCUPIED
+        return 1 if neighbors.zero? && current == EMPTY
+        0
+    end
+
+    def count_neighbors(x, y)
+        y_values = [y]
+        y_values << y - 1 unless y.zero?
+        y_values << y + 1 unless y == input.length - 1
+        x_values = [x]
+        x_values << x - 1 unless x.zero?
+        x_values << x + 1 unless x == input[y].length - 1
+        values = []
+        y_values.each do |y_val|
+            x_values.each do |x_val|
+                values << input[y_val][x_val] unless y_val == y && x_val == x
+            end
+        end
+        values.count { _1 == OCCUPIED }
     end
 end
--- spec/solutions/day_eleven_spec.rb
+++ spec/solutions/day_eleven_spec.rb
@@ -7,7 +7,7 @@ RSpec.describe DayEleven do
 
         context 'part 1' do
             let(:part) { 1 }
-            it { is_expected.to eq 0 }
+            it { is_expected.to eq 2427 }
         end
 
         context 'when part 2' do
@@ -15,17 +15,109 @@ RSpec.describe DayEleven do
         end
     end
 
-    let(:input) { %w[hello] }
+    let(:input) { %w[L.LL.LL.LL
+        LLLLLLL.LL
+        L.L.L..L..
+        LLLL.LL.LL
+        L.LL.LL.LL
+        L.LLLLL.LL
+        ..L.L.....
+        LLLLLLLLLL
+        L.LLLLLL.L
+        L.LLLLL.LL] }
+    let(:part) { 1 }
+
     describe '#process' do
         subject { described_class.new(input, part).process }
 
         context 'part 1' do
             let(:part) { 1 }
-            it { is_expected.to eq 'hello' }
+
+            it { is_expected.to eq 37 }
         end
 
         context 'when part 2' do
             let(:part) { 2 }
+        end
+    end
+
+    describe '#process_seat_map' do
+        let(:instance) { described_class.new(input, part) }
+        subject { instance.process_seat_map }
+
+        it 'updates the seat map' do
+            subject
+           expect(instance.input).to eq %w[#.##.##.##
+            #######.##
+            #.#.#..#..
+            ####.##.##
+            #.##.##.##
+            #.#####.##
+            ..#.#.....
+            ##########
+            #.######.#
+            #.#####.##]
+        end
+
+        context 'second round' do
+            let(:input) { %w[#.##.##.##
+                #######.##
+                #.#.#..#..
+                ####.##.##
+                #.##.##.##
+                #.#####.##
+                ..#.#.....
+                ##########
+                #.######.#
+                #.#####.##] }
+
+            
+            it 'updates the seat map' do
+                subject
+                expect(instance.input).to eq %w[#.LL.L#.##
+                    #LLLLLL.L#
+                    L.L.L..L..
+                    #LLL.LL.L#
+                    #.LL.LL.LL
+                    #.LLLL#.##
+                    ..L.L.....
+                    #LLLLLLLL#
+                    #.LLLLLL.L
+                    #.#LLLL.##]
+            end
+        end
+    end
+
+    describe '#seat_transformation' do
+        subject { described_class.new(input, part).seat_transformation(x, y) }
+        
+        context 'first seat' do
+            let(:x) { 0 }
+            let(:y) { 0 }
+
+            it { is_expected.to eq 1 }
+        end
+    end
+
+    describe '#count_neighbors' do
+        subject { described_class.new(input, part).count_neighbors(x, y) }
+        
+        context 'first seat' do
+            let(:x) { 0 }
+            let(:y) { 0 }
+            let(:input) { %w[L.LL.LL.L#] }
+
+            it { is_expected.to eq 0 }
+        end
+
+        context 'occupied seat' do
+            let(:input) { %w[#.LL.L#.##
+                             #LLLLLL.L#
+                             L.L.L..L..] }
+            let(:x) { 1 }
+            let(:y) { 1 }
+
+            it { is_expected.to eq 2 }
         end
     end
 end
